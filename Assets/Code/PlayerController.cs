aeab02093e906baa87d28b571bdb042dc350f1d4
DESKTOP-4T6OIJ7\Naomi <naomi.morandin@gmail.com>
2025-11-06 14:06:05 +1030

    Updated Animation and Added Movement

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -1,23 +1,37 @@
-    using UnityEngine;
+using UnityEngine;
 
-    public class CharacterMovement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Animator animator;
+    private Rigidbody2D rb;
+    private float moveInput;
+
+    void Start()
     {
-        public float moveSpeed = 5f;
-        private Animator animator;
-
-        void Start()
-        {
-            animator = GetComponent<Animator>();
-        }
-
-        void Update()
-        {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-
-            Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
-            float currentSpeed = movement.magnitude;
-
-            animator.SetFloat("Speed", currentSpeed); // "Speed" is the parameter name in the Animator
-        }
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
+
+    void Update()
+    {
+        // Get A/D input (mapped to Horizontal axis)
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        // Update animator based on speed (absolute value)
+        animator.SetFloat("Speed", Mathf.Abs(moveInput));
+    }
+
+    void FixedUpdate()
+    {
+        // Move character left/right
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        // Flip sprite based on direction
+        if (moveInput > 0)
+            transform.localScale = new Vector3(1, 1, 1); // facing right
+        else if (moveInput < 0)
+            transform.localScale = new Vector3(-1, 1, 1); // facing left
+    }
+}
