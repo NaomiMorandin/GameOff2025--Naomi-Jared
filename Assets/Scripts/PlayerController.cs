893365822f9b2608e2ed744a7ad68e1c4301664c
DESKTOP-4T6OIJ7\Naomi <naomi.morandin@gmail.com>
2025-11-19 11:03:13 +1030

    Revert "Merge branch 'Naomi-Feature-Branch'"
    This reverts commit 16f97173986584b185dd2d39ef1211700633992e, reversing
    changes made to f0dac6c8a70c57cca029adea64f564f8b90169f9.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,173 +1,23 @@
-using UnityEngine;
+    using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class PlayerController : MonoBehaviour
-{
-    [Header("Movement")]
-    public float walkSpeed = 3f;
-    public float flightSpeed = 5f;
-    public float ascendSpeed = 2f;
-    public float glideFallSpeed = 1f;
-    public float fastFallSpeed = 8f;
-
-    [Header("Stats")]
-    public float maxHP = 50f;
-    public float maxSP = 100f;
-
-    public float hp;   // runtime
-    public float sp;
-
-    [Header("HP Rates")]
-    public float fireDamageRate = 5f;   // HP lost per second
-    public float healRate = 5f;         // HP gained per second
-
-    [Header("SP Rates")]
-    public float flightDrainRate = 2f;  // per second
-    public float glideDrainRate = 1f;   // per second
-    public float groundedRegenRate = 5f;// per second
-    private bool inFireZone = false;
-    private bool inHQZone = false;
-
-    private bool isGrounded = false;
-    private bool canFly => sp > 0;
-    private bool staminaLock => sp <= 0;
-
-    private Animator animator;
-    private Rigidbody2D rb;
-
-    private float moveInput;
-    private bool upHeld;
-    private bool downHeld;
-
-    void Start()
+    public class CharacterMovement : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
+        public float moveSpeed = 5f;
+        private Animator animator;
 
-        hp = maxHP;
-        sp = maxSP;
-    }
-
-    void Update()
-    {
-        moveInput = Input.GetAxisRaw("Horizontal");
-        upHeld = Input.GetKey(KeyCode.W);
-        downHeld = Input.GetKey(KeyCode.S);
-
-        animator.SetFloat("Speed", Mathf.Abs(moveInput));
-
-        HandleHP();
-        HandleSP();
-    }
-
-    void FixedUpdate()
-    {
-        HandleMovement();
-    }
-
-    // -------------------------
-    //        HEALTH
-    // -------------------------
-    void HandleHP()
-    {
-        if (inFireZone)
-            hp -= fireDamageRate * Time.deltaTime;
-
-        if (inHQZone)
-            hp += healRate * Time.deltaTime;
-
-        hp = Mathf.Clamp(hp, 0, maxHP);
-    }
-
-    // -------------------------
-    //        STAMINA
-    // -------------------------
-    void HandleSP()
-    {
-        if (!isGrounded) // airborne
+        void Start()
         {
-            if (upHeld && !staminaLock)
-                sp -= flightDrainRate * Time.deltaTime;
-
-            else if (!downHeld) // gliding
-                sp -= glideDrainRate * Time.deltaTime;
-        }
-        else // grounded
-        {
-            sp += groundedRegenRate * Time.deltaTime;
+            animator = GetComponent<Animator>();
         }
 
-        sp = Mathf.Clamp(sp, 0, maxSP);
-    }
-
-    // -------------------------
-    //        MOVEMENT
-    // -------------------------
-    void HandleMovement()
-    {
-        float horizontalSpeed = isGrounded ? walkSpeed : flightSpeed;
-
-        // Basic left/right
-        rb.linearVelocity = new Vector2(moveInput * horizontalSpeed, rb.linearVelocity.y);
-
-        // Flying / Ascending
-        if (upHeld && !staminaLock)
+        void Update()
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, ascendSpeed);
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+            float currentSpeed = movement.magnitude;
+
+            animator.SetFloat("Speed", currentSpeed); // "Speed" is the parameter name in the Animator
         }
-        // Gliding
-        else if (!isGrounded && !downHeld)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, -glideFallSpeed);
-        }
-        // Fast fall
-        else if (downHeld)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, -fastFallSpeed);
-        }
-
-        // Flip sprite
-        if (moveInput > 0)
-            transform.localScale = new Vector3(1, 1, 1);
-        else if (moveInput < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
     }
-
-    // -------------------------
-    // DETECT GROUND
-    // -------------------------
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.collider.CompareTag("Ground"))
-            isGrounded = true;
-    }
-
-    void OnCollisionExit2D(Collision2D col)
-    {
-        if (col.collider.CompareTag("Ground"))
-            isGrounded = false;
-    }
-
-    // -------------------------
-    // FIRE & HQ ZONES
-    // -------------------------
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag("Fire"))
-            inFireZone = true;
-
-        if (col.CompareTag("HQ"))
-            inHQZone = true;
-    }
-
-    void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.CompareTag("Fire"))
-            inFireZone = false;
-
-        if (col.CompareTag("HQ"))
-            inHQZone = false;
-    }
-
-
-}
